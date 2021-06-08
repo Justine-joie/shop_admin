a422117db566f26b21921639c50691dbe5e39055
Justine-joie <2463209371@qq.com>
2021-06-08 14:07:54 +0800

    初始化

---
+++ config/defaultSettings.js
@@ -0,0 +1,14 @@
+const proSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#ff9100',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '今日商城',
+  pwa: false,
+  iconfontUrl: '',
+};
+export default proSettings;
